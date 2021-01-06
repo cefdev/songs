191bc8d9dfe62786d7a2b640816e5ae9ba4c0008
Moncef <moncef.koumila@gmail.com>
2021-01-06 12:46:16 +0000

    Combine reducers using Redux's 'combineReducers'

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from "redux";
+
 const songsReducer = () => {
   return [
     { title: "God's Country", duration: "4:12" },
@@ -14,3 +16,8 @@ const seletedSongReducer = (selectedSong = null, action) => {
 
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: seletedSongReducer,
+});

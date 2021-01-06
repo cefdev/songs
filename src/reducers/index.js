f4db6e0b287d82ee3e73c65c6f22f671dff0bd70
Moncef <moncef.koumila@gmail.com>
2021-01-06 12:25:23 +0000

    Building reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+const songsReducer = () => {
+  return [
+    { title: "God's Country", duration: "4:12" },
+    { title: "One More Time", duration: "3:56" },
+    { title: "Fire", duration: "4:03" },
+    { title: "Bloods On My Name", duration: "2:47" },
+  ];
+};
+
+const seletedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.type;
+  }
+
+  return selectedSong;
+};
